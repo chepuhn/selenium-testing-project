081d8fca5cde9b08d485035326787da8d8d8ac14
Пахлебухин Н.Г. <220268@edu.psuti.ru>
2026-05-18 23:15:16 +0300

    Fix GitHub Actions Selenium tests

--- conftest.py
+++ conftest.py
@@ -1,11 +1,19 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 
 @pytest.fixture
 def driver():
-    driver = webdriver.Chrome()
-    driver.maximize_window()
+    options = Options()
+    options.add_argument("--headless=new")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--window-size=1920,1080")
+
+    driver = webdriver.Chrome(options=options)
     driver.implicitly_wait(5)
+
     yield driver
+
     driver.quit()
