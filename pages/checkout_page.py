ee154c531e3daca9dabc37a097d3909aa0dd2e71
Пахлебухин Н.Г. <220268@edu.psuti.ru>
2026-05-18 23:23:31 +0300

    Fix checkout page wait

--- pages/checkout_page.py
+++ pages/checkout_page.py
@@ -1,4 +1,7 @@
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 from pages.base_page import BasePage
 
 
@@ -11,13 +14,25 @@ class CheckoutPage(BasePage):
     COMPLETE_HEADER = (By.CLASS_NAME, "complete-header")
 
     def fill_user_data(self, first_name, last_name, postal_code):
+        WebDriverWait(self.driver, 10).until(
+            EC.presence_of_element_located(self.FIRST_NAME_INPUT)
+        )
+
         self.type_text(self.FIRST_NAME_INPUT, first_name)
         self.type_text(self.LAST_NAME_INPUT, last_name)
         self.type_text(self.POSTAL_CODE_INPUT, postal_code)
         self.click(self.CONTINUE_BUTTON)
 
     def finish_order(self):
+        WebDriverWait(self.driver, 10).until(
+            EC.element_to_be_clickable(self.FINISH_BUTTON)
+        )
+
         self.click(self.FINISH_BUTTON)
 
     def get_complete_message(self):
+        WebDriverWait(self.driver, 10).until(
+            EC.presence_of_element_located(self.COMPLETE_HEADER)
+        )
+
         return self.get_text(self.COMPLETE_HEADER)
